fc8d79edc92e15823f80af3ba69bb42a44001fcb
Ching Pei Yang <badnam3o.0@gmail.com>
2024-10-28 15:35:48 +0100

    termux-auth: successful build

--- packages/termux-auth.nix
+++ packages/termux-auth.nix
@@ -1,10 +1,16 @@
 {
   src,
   cmake,
+  openssl,
   stdenv,
 }:
 stdenv.mkDerivation {
+  pname = "termux-auth";
+  version = "git";
   inherit src;
 
+  patches = [./termux-auth.patch];
+
   nativeBuildInputs = [cmake];
+  buildInputs = [openssl];
 }
