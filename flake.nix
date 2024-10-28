3f1bbbc6ae7383fac12fc44dfbd15815cbdf6267
Ching Pei Yang <badnam3o.0@gmail.com>
2024-10-28 15:44:13 +0100

    openssh: init

--- flake.nix
+++ flake.nix
@@ -4,6 +4,8 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     termux-auth.url = "github:termux/termux-auth";
     termux-auth.flake = false;
+    termux-packages.url = "github:termux/termux-packages";
+    termux-packages.flake = false;
   };
   outputs = {
     self,
@@ -21,14 +23,18 @@
         }
     );
   in {
+    inherit inputs;
     overlays = {
       default = final: prev: {
         termux-auth = final.callPackage ./packages/termux-auth.nix {src = inputs.termux-auth;};
+        openssh = final.callPackage ./packages/openssh.nix {
+          termuxPkg = "${inputs.termux-packages}/packages/openssh";
+        };
       };
     };
 
     packages = eachSystem (system: {
-      inherit (pkgsFor.${system}) termux-auth;
+      inherit (pkgsFor.${system}) termux-auth openssh;
     });
   };
 }
---
+++ packages/openssh.nix
@@ -0,0 +1,32 @@
+{
+  termuxPkg,
+  openssh,
+}:
+openssh.override {
+  extraPatches = [
+    "${termuxPkg}/auth.c.patch"
+    "${termuxPkg}/auth-passwd.c.patch"
+    "${termuxPkg}/contrib_ssh-copy-id.patch"
+    "${termuxPkg}/defines.h.patch"
+    "${termuxPkg}/hostfile.c.patch"
+    "${termuxPkg}/loginrec.c.patch"
+    "${termuxPkg}/Makefile.in.patch"
+    "${termuxPkg}/misc.c.patch"
+    "${termuxPkg}/mux.c.patch"
+    "${termuxPkg}/openbsd-compat_explicit_bzero.c.patch"
+    "${termuxPkg}/openbsd-compat_xcrypt.c.patch"
+    "${termuxPkg}/pathnames.h.patch"
+    "${termuxPkg}/readconf.c.patch"
+    "${termuxPkg}/servconf.c.patch"
+    "${termuxPkg}/session.c.patch"
+    "${termuxPkg}/sftp-server.c.patch"
+    "${termuxPkg}/ssh-agent.c.patch"
+    "${termuxPkg}/ssh_config.patch"
+    "${termuxPkg}/sshd_config.5.patch"
+    "${termuxPkg}/sshd_config.patch"
+    "${termuxPkg}/sshd.c.patch"
+    "${termuxPkg}/sshd-session.c.patch"
+    "${termuxPkg}/ssh-keygen.c.patch"
+    "${termuxPkg}/sshpty.c.patch"
+  ];
+}
