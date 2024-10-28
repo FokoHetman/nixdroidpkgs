774a4d5c63f4f3b346bebdea7b09eba9bcb04d47
Ching Pei Yang <badnam3o.0@gmail.com>
2024-10-28 18:12:44 +0100

    setupHooks: setup env vars and cflags

--- packages/termux-patch-setup-hooks.sh
+++ packages/termux-patch-setup-hooks.sh
@@ -1,4 +1,11 @@
 
+TERMUX_APP_PACKAGE=${TERMUX_APP_PACKAGE:-com.termux.nix}
+TERMUX_BASE_DIR="/data/data/${TERMUX_APP_PACKAGE}/files"
+TERMUX_CACHE_DIR="/data/data/${TERMUX_APP_PACKAGE}/cache"
+TERMUX_ANDROID_HOME="${TERMUX_BASE_DIR}/home"
+TERMUX_PREFIX_CLASSICAL="${TERMUX_BASE_DIR}/usr"
+TERMUX_PREFIX="${TERMUX_PREFIX_CLASSICAL}"
+
 termux_step_patch_package() {
 	shopt -s nullglob
 	for patch in $termuxPatches; do
@@ -16,3 +23,11 @@ termux_step_patch_package() {
 }
 
 preConfigurePhases+=(termux_step_patch_package)
+
+appendToVar CFLAGS -D__ANDROID__
+appendToVar CFLAGS "-DTERMUX_APP_PACKAGE=\\\"${TERMUX_APP_PACKAGE}\\\""
+appendToVar CFLAGS "-DTERMUX_BASE_DIR=\\\"${TERMUX_BASE_DIR}\\\""
+appendToVar CFLAGS "-DTERMUX_CACHE_DIR=\\\"${TERMUX_CACHE_DIR}\\\""
+appendToVar CFLAGS "-DTERMUX_HOME=\\\"${TERMUX_ANDROID_HOME}\\\""
+appendToVar CFLAGS "-DTERMUX_PREFIX=\\\"${TERMUX_PREFIX}\\\""
+appendToVar CFLAGS "-DTERMUX_PREFIX_CLASSICAL=\\\"${TERMUX_PREFIX_CLASSICAL}\\\""
